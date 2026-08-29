0608973ea85703e64c0e30f039364e0d39cd1496
ziazeshan141@outlook.com <ziazeshan141@outlook.com>
2026-08-29 23:35:17 +0300

    Add AWS Load Balancer Controller

--- infra/terraform/modules/eks/main.tf
+++ infra/terraform/modules/eks/main.tf
@@ -1,4 +1,57 @@
 # ============================================================
+# AWS LOAD BALANCER CONTROLLER
+# ============================================================
+
+data "aws_iam_policy_document" "load_balancer_controller_assume_role" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRole",
+      "sts:TagSession"
+    ]
+
+    principals {
+      type = "Service"
+
+      identifiers = [
+        "pods.eks.amazonaws.com"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "load_balancer_controller" {
+  name = "${var.cluster_name}-aws-load-balancer-controller"
+
+  assume_role_policy = data.aws_iam_policy_document.load_balancer_controller_assume_role.json
+
+  tags = var.tags
+}
+
+resource "aws_iam_policy" "load_balancer_controller" {
+  name = "${var.cluster_name}-AWSLoadBalancerControllerIAMPolicy"
+
+  policy = file("${path.module}/aws-load-balancer-controller-iam-policy.json")
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "load_balancer_controller" {
+  role       = aws_iam_role.load_balancer_controller.name
+  policy_arn = aws_iam_policy.load_balancer_controller.arn
+}
+
+resource "aws_eks_pod_identity_association" "load_balancer_controller" {
+  cluster_name = var.cluster_name
+
+  namespace       = "kube-system"
+  service_account = "aws-load-balancer-controller"
+
+  role_arn = aws_iam_role.load_balancer_controller.arn
+}
+
+# ============================================================
 # EKS Access Entries
 # ============================================================
 
